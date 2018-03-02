fb4d6558937e2d5338de8056ec66d90adfe26224
github username <github email address>
2018-03-01 23:24:41 -0500

    Updated recipes show to display jQuery comments

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -58,19 +58,24 @@ $(document).ready(function() {
 $(document).ready(function() {
   if ($(".recipes.show").length) {
     $("#recipeIngredients").empty()
+    $("#recipeComments").empty()
     recipe_string = "/recipes/" + $('h1').data("id");
     $.getJSON(recipe_string, function(recipe){
-      console.log(recipe)
       $(".recipeName").text(recipe.name);
       $(".recipeInstructions").text(recipe.instructions);
       $(".recipeDuration").text(recipe.duration);
-      $.each(recipe.recipe_ingredients, function(key, recipe_ingredient ){
+      $.each(recipe.recipe_ingredients, function(key, element){
+        var recipe_ingredient = element
         content = '<tr>'
         content += '<td>' + recipe_ingredient.ingredient_name + '</td>';
         content += '<td>' + recipe_ingredient.quantity + '</td>';
         content += '<td>' + recipe_ingredient.unit+ '</td>';
         content += '</tr>'
         $(content).appendTo("#recipeIngredients");
+      });
+      $.each(recipe.comments, function(key, element){
+        var comment = new Comment(element);
+        comment.displayComment();
       });
     });
   };
@@ -79,12 +84,13 @@ $(document).ready(function() {
 function Comment(data) {
   this.id = data.id;
   this.recipe_id = data.recipe_id
-  this.user = data.user
+  this.user_id = data.user_id
   this.body = data.body;
+  this.user_email = data.user_email
 }
 
 Comment.prototype.displayComment = function() {
   var content = "" ;
-  content += "<p><hr><b>User:</b>" + this.user.email + "</p><p><b>Comment:</b>" + this.body + "</p>";
-  $("#submitted-comments").append(html);
+  content += "<p><hr><b>User:</b> " + this.user_email + "</p><p><b>Comment:</b> " + this.body + "</p>";
+  $("#recipeComments").append(content);
 }
